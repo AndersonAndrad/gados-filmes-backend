a5cfb4a79d0c0bd4e15b9def2cbb47e849b39d87
Ygor Fortes <ygor1888@hotmail.com>
2024-01-13 23:12:44 -0300

    feat: created method login in controller and call at contructor in auth.controller

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,28 @@
+import { CrudControllerUtils } from '../../utils/crud/crud-controller.utils.js'
+import { AuthValidatorSchema } from './validators/auth.validator.schema.js'
+import { AuthService } from './services/auth.services.js'
+import { VerificationTokenMiddleware } from '../../middlewares/verification.token.middleware.js'
+
+export class AuthController extends CrudControllerUtils {
+  constructor () {
+    super()
+    this.verificationTokenMiddleware = new VerificationTokenMiddleware()
+    this.authService = new AuthService()
+    this.authvalidatorSchema = new AuthValidatorSchema()
+    this.setupRouter(this.login())
+  }
+
+  login () {
+    this.router.post('/login', async (req, res, next) => {
+      try {
+        const loginBodyValidated = await this.authvalidatorSchema.login(req.body)
+
+        const login = await this.authService.login({ ...loginBodyValidated })
+
+        return res.status(200).send(login)
+      } catch (error) {
+        next(error)
+      }
+    })
+  }
+}
